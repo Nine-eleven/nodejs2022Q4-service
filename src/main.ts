6e18e565e2ca6394ebbcd80d040a4fd28cc1f541
Denis Fedorov <denis.fedorov@intexsoft.by>
2023-02-06 18:59:14 +0300

    feat: API document file edited, and enable CORS

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ dotenv.config();
 async function bootstrap() {
   const PORT = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ErrorInterceptor());
